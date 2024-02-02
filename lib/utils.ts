c62a684451381b35b7e671ada44d0169b5591f59
Stiven Castillo <stivencastillo.90@gmail.com>
2024-02-02 11:02:59 -0500

    remove comments

--- lib/utils.ts
+++ lib/utils.ts
@@ -31,10 +31,10 @@ export const capitalize = (str: string) => {
 
 export const slugify = (text: string) => {
   return text.toString().toLowerCase()
-    .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-    .replace(/^-+/, '')             // Trim - from start of text
-    .replace(/-+$/, '')             // Trim - from end of text
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
     .normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
